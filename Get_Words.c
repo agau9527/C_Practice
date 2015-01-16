5763b5152708a2bcf193c5cea8fa01ef3bcd1b60
agau9527 <agau9527@gmail.com>
2015-01-16 11:19:41 +0800

    update

---
+++ Get_Words.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int Get_Words(const char sentence[ ])
+{
+    int counted = 0; // result
+
+    // state:
+    const char* it = sentence;
+    int inword = 0;
+
+    do switch(*it) {
+        case '\0':
+        case ' ': case '\t': case '\n': case '\r': // TODO others?
+            if (inword) { inword = 0; counted++; }
+            break;
+        default: inword = 1;
+    } while(*it++);
+
+    return counted;
+}
